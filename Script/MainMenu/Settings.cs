28540f51c2afc6e7649f39de46788c4e2f460e2e
AlexandraLCruz <alexandralcruz@iskolarngbayan.pup.edu.ph>
2025-06-20 01:05:54 +0800

    save volume in json

--- Script/MainMenu/Settings.cs
+++ Script/MainMenu/Settings.cs
@@ -3,43 +3,96 @@ using System;
 
 public partial class Settings : Control
 {
-    [Export] public NodePath BackButtonPath;
-    [Export] public NodePath VolumeSliderPath;
+	[Export] public NodePath BackButtonPath;
+	[Export] public NodePath VolumeSliderPath;
 
-    private Button backButton;
-    private HSlider volumeSlider;
+	private Button backButton;
+	private HSlider volumeSlider;
 
-    // Name of your custom bus (must match the name in the Audio panel)
-    private const string MusicBusName = "Master";
+	// Name of your custom bus (must match the name in the Audio panel)
+	private const string MusicBusName = "Master";
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        backButton = GetNode<Button>(BackButtonPath);
-        backButton.Pressed += OnBackButtonPressed;
+	
+	// Signal for when the volume changes, to notify other scripts (e.g., MainGame)
+	[Signal]
+	public delegate void VolumeChangedEventHandler(float newVolume);
+	
 
-        volumeSlider = GetNode<HSlider>(VolumeSliderPath);
-        volumeSlider.ValueChanged += OnVolumeChanged;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		backButton = GetNode<Button>(BackButtonPath);
+		backButton.Pressed += OnBackButtonPressed;
 
-        // Initialize slider to current volume of the custom bus
-        int busIdx = AudioServer.GetBusIndex(MusicBusName);
-        volumeSlider.Value = AudioServer.GetBusVolumeDb(busIdx);
-    }
+		volumeSlider = GetNode<HSlider>(VolumeSliderPath);
+		volumeSlider.ValueChanged += OnVolumeChanged;
 
-    private void OnVolumeChanged(double value)
-    {
-        int busIdx = AudioServer.GetBusIndex(MusicBusName);
-        AudioServer.SetBusVolumeDb(busIdx, (float)value);
-    }
+		
+		// Load saved volume if available, otherwise use current bus volume
+		float initialVolume = 0f;
+		if (FileAccess.FileExists("user://savegame.json"))
+		{
+			using var file = FileAccess.Open("user://savegame.json", FileAccess.ModeFlags.Read);
+			var json = file.GetAsText();
+			var result = Json.ParseString(json);
+			var saveData = result.As<Godot.Collections.Dictionary>();
+			if (saveData != null && saveData.ContainsKey("volume"))
+				initialVolume = (float)saveData["volume"];
+			else
+			{
+				int busIdx = AudioServer.GetBusIndex(MusicBusName);
+				initialVolume = AudioServer.GetBusVolumeDb(busIdx);
+			}
+		}
+		else
+		{
+			int busIdx = AudioServer.GetBusIndex(MusicBusName);
+			initialVolume = AudioServer.GetBusVolumeDb(busIdx);
+		}
+		volumeSlider.Value = initialVolume;
+	   
+	}
 
-    private void OnBackButtonPressed()
-    {
-        QueueFree();
-    }
+	private void OnVolumeChanged(double value)
+	{
+		int busIdx = AudioServer.GetBusIndex(MusicBusName);
+		if (busIdx >= 0)
+			AudioServer.SetBusVolumeDb(busIdx, (float)value);
+		else
+			GD.PrintErr($"Audio bus '{MusicBusName}' not found!");
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        
-    }
-}
+		
+		// Save volume to savegame.json so it persists between scenes and sessions
+		Godot.Collections.Dictionary saveData;
+		if (FileAccess.FileExists("user://savegame.json"))
+		{
+			using var file = FileAccess.Open("user://savegame.json", FileAccess.ModeFlags.Read);
+			var json = file.GetAsText();
+			var result = Json.ParseString(json);
+			saveData = result.As<Godot.Collections.Dictionary>() ?? new Godot.Collections.Dictionary();
+		}
+		else
+		{
+			saveData = new Godot.Collections.Dictionary();
+		}
+		saveData["volume"] = (float)value;
+		string newJson = Json.Stringify(saveData);
+		using var writeFile = FileAccess.Open("user://savegame.json", FileAccess.ModeFlags.Write);
+		writeFile.StoreString(newJson);
+
+		// Emit the signal so other scripts (like MainGame) can react to the volume change
+		EmitSignal(nameof(VolumeChangedEventHandler), (float)value);
+	  
+	}
+
+	private void OnBackButtonPressed()
+	{
+		QueueFree();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+} 
