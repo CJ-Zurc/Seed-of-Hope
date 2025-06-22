962c5d4e185d4a3ec503757c039dfd0c3d711cce
CJZurc <cruz.cristianjames.ambrocio@gmail.com>
2025-06-22 15:28:18 +0800

    re-commit of mouse handler

--- Script/Inventory/InventoryPanel.cs
+++ Script/Inventory/InventoryPanel.cs
@@ -4,20 +4,33 @@ using System;
 public partial class InventoryPanel : PanelContainer
 {
 	private Button waterButton;
+	private Texture2D defaultCursor;
 
     public override void _Ready()
 	{
-        // Connects the water button to the OnWaterButtonPressed method
-        // This will change the mouse cursor to a watering can when the button is pressed
+
+        defaultCursor = GD.Load<Texture2D>("res://2D Arts/UI stuff/Buttons/Triangle Mouse icon 1_64x64.png");
+        Input.SetCustomMouseCursor(defaultCursor, Input.CursorShape.Arrow, Vector2.Zero);
+
         waterButton = GetNode<Button>("/root/MainGame/HUD/Control/inventoryPanel/MarginContainer/HBoxContainer/waterCan");
         waterButton.Pressed += OnWaterButtonPressed;
 
     }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton mouseEvent && mouseEvent.ButtonIndex == MouseButton.Right && mouseEvent.Pressed)
+        {
+            // Reset the mouse cursor to the default custom icon when right mouse button is pressed
+            Input.SetCustomMouseCursor(defaultCursor, Input.CursorShape.Arrow, Vector2.Zero);
+        }
+    }
+
 	private void OnWaterButtonPressed()
 	{
         //loads the watering can texture and sets it as the custom mouse cursor
         Texture2D wateringCan = GD.Load<Texture2D>("res://2D Arts/GardenAssets/wateringCan.png");
-        // Sets the custom mouse cursor to the watering can texture
+ 
         Input.SetCustomMouseCursor(wateringCan, Input.CursorShape.Arrow, Vector2.Zero);
     }
  
