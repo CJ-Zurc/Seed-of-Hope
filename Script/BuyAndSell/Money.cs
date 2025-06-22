bedd09653b871e3621f2984eb4c4d7d32e46dc75
CJZurc <cruz.cristianjames.ambrocio@gmail.com>
2025-06-22 22:07:08 +0800

    added function to the optionbutton for HUD

--- Script/BuyAndSell/Money.cs
+++ Script/BuyAndSell/Money.cs
@@ -4,19 +4,30 @@ using System;
 public partial class Money : CanvasLayer
 {
 	private Label moneyLabel;
-    public override void _Ready()
+	private Button optionButton;
+
+	
+	public override void _Ready()
 	{
 		moneyLabel = GetNode<Label>("Control/moneyContainer/money");
 		var moneyManager = GetNode<MoneyManager>("/root/MoneyManager");
+		optionButton = GetNode<Button>("Control/optionButton");
 		moneyLabel.Text = moneyManager.CurrentMoney.ToString();
+		optionButton.Pressed += OnOptionButtonPressed;
 
-        // Connect the MoneyChanged signal to update the label when money changes
+		// Connect the MoneyChanged signal to update the label when money changes
 		moneyManager.MoneyChanged += OnMoneyChanged;
-    }
+	}
 
 	private void OnMoneyChanged(int newAmount)
 	{
 		moneyLabel.Text = newAmount.ToString();
 	}
 
+	private void OnOptionButtonPressed()
+	{
+		var settingsScene = GD.Load<PackedScene>("res://scenes/settings.tscn");
+		var settingsInstance = settingsScene.Instantiate();
+		AddChild(settingsInstance);
+	}
 }
