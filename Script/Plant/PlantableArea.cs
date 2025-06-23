abbd9ad71426a49e376f64c25fa27c11989a0149
CJZurc <cruz.cristianjames.ambrocio@gmail.com>
2025-06-23 20:17:47 +0800

    Added Comments

--- Script/Plant/PlantableArea.cs
+++ Script/Plant/PlantableArea.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 
 public abstract partial class PlantableArea : Area2D
 {
-    // Grouped variables for saving/loading
+	// Grouped variables for saving/loading
+	// I think ito yung mga kailangan i-save sa plant cinompile ko na pero test nyo if ever ayaw gumana	
     public abstract string UniqueID { get; }
     public abstract string CurrentSeedType { get; }
     public abstract int GrowthDaysLeft { get; }
